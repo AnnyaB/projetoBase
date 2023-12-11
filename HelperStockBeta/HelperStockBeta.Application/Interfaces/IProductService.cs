0f49c3c6aea34fb5206884370a1950df70958724
Ana Bordin <118741376+AnnyaB@users.noreply.github.com>
2023-12-11 15:02:21 -0300

    avatp2-AnaBordin

---
+++ HelperStockBeta/HelperStockBeta.Application/Interfaces/IProductService.cs
@@ -0,0 +1,20 @@
+﻿using HelperStockBeta.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperStockBeta.Application.Interfaces
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDTO>> GetProducts();
+        Task<IEnumerable<ProductDTO>> GetProductByCategory(int idCategory);
+        Task<ProductDTO> GetProductById(int? id);
+        Task Add(ProductDTO productDTO);
+        Task Update(ProductDTO productDTO);
+        Task Remove(int? id);
+
+    }
+}
